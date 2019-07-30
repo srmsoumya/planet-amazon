c6015f36347d922ea1b8c9d1fbabe52fa369836d
manohar <kmanoharmurali@gmail.com>
2019-07-30 12:51:09 +0530

    CookieCutter Template until Docker

---
+++ CookieCutter_ProDL/{{cookiecutter.project_name}}/predict.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append('./python_scripts/')
+
+def load_model(*args):
+    '''Load and return the model from the checkpoint/ dir.'''
+    pass
+
+def load_data(input):
+    '''Perform the required pre-processing on the given input required for the model and return.'''
+    pass
+
+def predict(model, input):
+    '''Pass the input to the model and return the result.'''
+    pass
+
